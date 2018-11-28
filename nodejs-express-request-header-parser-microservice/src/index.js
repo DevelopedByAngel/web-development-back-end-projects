441578e6e4ee8a7f07bad3680c48d5f105579aab
niccololampa <niccololampa@gmail.com>
2018-11-28 20:47:53 +0800

    working-express-request-parser

--- nodejs-express-request-header-parser-microservice/src/index.js
+++ nodejs-express-request-header-parser-microservice/src/index.js
@@ -4,17 +4,32 @@
 
 /* Run on http://localhost:8080/ after node index.js */
 
-var express = require('express');
-
-var app = express();
+const express = require('express');
+const app = express();
 
 // connect to port 8080
 app.listen(8080);
 
+app.set('trust proxy', true);
+
+// app.enable('trust proxy');
+
 // serve styling static assets
+app.use(express.static(__dirname + '/styling'));
 
-// app.use(express.static(__dirname + 'styling'));
+// // get projectInfo for home page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/project-info.html');
+});
 
-// app.get('/', function(req, res) {});
+//to get info
+app.get('/api/whoareyou/', (req, res) => {
+  let ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
 
-// dsaf;
+  if (ip.substr(0, 7) === '::ffff:') {
+    ip = ip.substr(7);
+  }
+  res.json({ ipaddress: ip, language: language, software: software });
+});

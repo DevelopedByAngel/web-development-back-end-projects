366e6203f69e054acf59be7d4946ea7019bfb69e
niccololampa <niccololampa@gmail.com>
2018-12-02 12:43:08 +0800

    updates-on-mongo-url-generator

--- mongo-express-node-url-shortener-microservice/src/index.js
+++ mongo-express-node-url-shortener-microservice/src/index.js
@@ -4,32 +4,21 @@
 
 /* Run on http://localhost:8080/ after node index.js */
 
+require('dotenv/config');
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-//function for time-stamp
-function timestampProvider(req, res) {
-  let dateInput = new Date(req.params.date_string);
-
-  // if date string is empty trigger current date.
-  if (req.params.date_string === undefined) {
-    dateInput = new Date();
-  }
-
-  /* if dateInput is invalid date try converting date string to UNIX to check if it will 
-  result to valid date*/
-  if (isNaN(dateInput.getTime())) {
-    dateInput = new Date(Number(req.params.date_string));
-  }
-
-  const unixInput = dateInput.getTime();
-  const utcInput = dateInput.toUTCString();
-
-  res.json({ unix: unixInput, utc: utcInput });
-}
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 app.listen(8080);
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true }
+);
 
 // //Serving static assets
 app.use(express.static(__dirname + '/styling'));
@@ -42,14 +31,71 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/project-info.html');
 });
 
-//generating timestamp from input post-request
-app.post('/api/timestamp/:date_string?', (req, res) =>
-  timestampProvider(req, res)
-);
+// create model schema
 
-// Json timestamp microservice
-app.get('/api/timestamp/:date_string?', (req, res) =>
-  timestampProvider(req, res)
-);
+const urlSchema = new Schema({
+  original_url: { type: String, required: true }
+});
+
+// upload to server
+
+const Urls = mongoose.model('Urls', urlSchema);
+
+// upload  urlprovided to database Method
+// let uploadNewLink = function(urlProvided, done) {
+//   const uploadedUrl = new Urls({ original_url: urlProvided });
+//   uploadedUrl.save((err, data) => {
+//     if (err) {
+//       return done(err);
+//     }
+//     return done(null, data);
+//   });
+// };
+
+// check validity of URL
+
+function validateUrl(url) {
+  var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  if (pattern.test(url)) {
+    alert('Url is valid');
+    return true;
+  }
+  alert('Url is not valid!');
+  return false;
+}
+
+app.post('/api/shorturl/', (req, res) => {
+  // get the url provided in form
+  const urlProvided = req.body.urlInput;
+  let id = null;
+
+  // if url is not valid
+  if (validateUrl(urlProvided)) {
+    res.json({ error: 'invalid URL' });
+  }
+
+  // uploadNewLink(urlProvided);
+
+  Urls.create({ original_url: urlProvided }),
+    (err, data) => {
+      if (err) return handleError(err);
+    };
+
+  // find url Provided id in database
+  id = Urls.findOne({ original_url: urlProvided }, (err, data) => {
+    if (err) return handleError(err);
+
+    if (data) {
+      console.log(data._id);
+      let id = data._id;
+      return;
+    }
+  });
+
+  let shorturl = urlProvided + '/' + id;
+
+  const output = { original_url: urlProvided, shorturl: shorturl };
+  res.json(output);
+});
 
 /*Coded by Niccolo Lampa. Email: niccololampa@gmail.com */

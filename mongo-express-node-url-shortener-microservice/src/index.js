c6a0fce76cfc1785e027261758ddf0e8d2a5ee0d
niccololampa <niccololampa@gmail.com>
2018-12-02 19:38:01 +0800

    updated-url-shortener

--- mongo-express-node-url-shortener-microservice/src/index.js
+++ mongo-express-node-url-shortener-microservice/src/index.js
@@ -1,9 +1,9 @@
 /*Coded by Niccolo Lampa. email:niccololampa@gmail.com */
 /* TO SEE  ALL FEATURES PLEASE VIEW IN CODESANDBOX FULL/SCREEN ACTUAL MODE * /
 /* PREVIEW MODE DOESN'T SHOW ALL FEATURES */
-
 /* Run on http://localhost:8080/ after node index.js */
 
+// for environment variables
 require('dotenv/config');
 
 var express = require('express');
@@ -20,7 +20,7 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-// //Serving static assets
+//Serving static assets
 app.use(express.static(__dirname + '/styling'));
 
 //to ready for post-request
@@ -32,70 +32,70 @@ app.get('/', (req, res) => {
 });
 
 // create model schema
-
 const urlSchema = new Schema({
   original_url: { type: String, required: true }
 });
 
 // upload to server
-
 const Urls = mongoose.model('Urls', urlSchema);
 
-// upload  urlprovided to database Method
-// let uploadNewLink = function(urlProvided, done) {
-//   const uploadedUrl = new Urls({ original_url: urlProvided });
-//   uploadedUrl.save((err, data) => {
-//     if (err) {
-//       return done(err);
-//     }
-//     return done(null, data);
-//   });
-// };
-
-// check validity of URL
-
+// FUNCTION TO CHECK IF URL IS VALID
 function validateUrl(url) {
   var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
   if (pattern.test(url)) {
-    alert('Url is valid');
     return true;
   }
-  alert('Url is not valid!');
   return false;
 }
 
+//  PROCESS POST REQUEST. WHEN URL IS SUBMITTED
 app.post('/api/shorturl/', (req, res) => {
   // get the url provided in form
-  const urlProvided = req.body.urlInput;
+  let urlProvided = req.body.urlInput;
   let id = null;
 
-  // if url is not valid
-  if (validateUrl(urlProvided)) {
-    res.json({ error: 'invalid URL' });
+  // Check validty of url. If  is not valid
+  if (validateUrl(urlProvided) === false) {
+    return res.json({ error: 'invalid URL' });
   }
 
-  // uploadNewLink(urlProvided);
-
+  // Upload submitted URL to Mongo database if url is valid
   Urls.create({ original_url: urlProvided }),
     (err, data) => {
       if (err) return handleError(err);
     };
 
-  // find url Provided id in database
-  id = Urls.findOne({ original_url: urlProvided }, (err, data) => {
+  // Once uploaded find the system generated ID of url. This will be used as the shortened url.
+  Urls.findOne({ original_url: urlProvided }, (err, data) => {
     if (err) return handleError(err);
 
     if (data) {
-      console.log(data._id);
-      let id = data._id;
-      return;
+      let id = data._id.toString();
+      let shorturl = req.get('host') + '/api/' + id;
+      const output = { original_url: urlProvided, short_url: shorturl };
+      // return processed
+      res.json(output);
     }
   });
+});
 
-  let shorturl = urlProvided + '/' + id;
+// SHORT URL REDIRECTING TO ORIGINAL URL
+app.get('/api/:urlInput', (req, res) => {
+  const personId = req.params.urlInput;
+  Urls.findById(personId, (err, data) => {
+    if (err) {
+      return handleError(err);
+    }
 
-  const output = { original_url: urlProvided, shorturl: shorturl };
-  res.json(output);
+    if (data) {
+      const orgUrl = data.original_url;
+
+      // redirect must include 301 request and http://
+      res.redirect(301, orgUrl);
+    }
+  });
 });
 
 /*Coded by Niccolo Lampa. Email: niccololampa@gmail.com */
+
+// localhost:8080/api/5c0286cdf0ab716df9eaaf40

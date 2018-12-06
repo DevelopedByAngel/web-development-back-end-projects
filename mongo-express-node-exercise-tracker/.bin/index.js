8f1878af0bada942300861e97282a515ba6eb01a
niccololampa <niccololampa@gmail.com>
2018-12-06 14:49:21 +0800

    final-updates-exercise-tracker

--- mongo-express-node-exercise-tracker/.bin/index.js
+++ mongo-express-node-exercise-tracker/.bin/index.js
@@ -3,6 +3,10 @@
 /* PREVIEW MODE DOESN'T SHOW ALL FEATURES */
 /* Run on http://localhost:8080/ after node index.js */
 
+// https://fuschia-custard.glitch.me/api/exercise/log?userId=rkGiwsQ14
+
+// https://fuschia-custard.glitch.me/api/exercise/add
+
 // for environment variables
 require('dotenv/config');
 
@@ -37,7 +41,7 @@ app.get('/', (req, res) => {
 const LogSchema = new Schema({
   description: { type: String },
   duration: { type: Number },
-  date: { type: String } // date will be type string as it is consistent yyyy-mm-dd format. No need to convert it into date type and deal with JSON date formatting issues. Client will handle the Date formatting
+  date: { type: Date }
 });
 
 // ------------------------------------------------------------------------------------------
@@ -69,22 +73,11 @@ app.post('/api/exercise/new-user', (req, res) => {
     return;
   }
 
-  // if username is valid then check if username already exist
-
-  Users.findOne({ username: newUserInput }, (err, data) => {
-    // if username already existing
-    if (data) {
-      res.send('User already existing');
-      return;
-    }
-    // username is not existing, save to the database
-    else {
-      const newUser = new Users({ username: newUserInput });
-      newUser.save(err => {
-        if (err) return console.error(err);
-        res.send({ username: newUserInput });
-      });
-    }
+  // if username is valid then upload to new User to database
+  const newUser = new Users({ username: newUserInput });
+  newUser.save(err => {
+    if (err) return console.error(err);
+    res.send({ username: newUserInput });
   });
 });
 
@@ -156,11 +149,36 @@ app.get('/api/exercise/log?', (req, res) => {
           // then go Filter array by date
           let filteredLogs = logs.filter(exerciseLog => {
             return (
-              new Date(exerciseLog.date) >= new Date(dateStartInput) &&
-              new Date(exerciseLog.date) <= new Date(dateEndInput)
+              exerciseLog.date >= new Date(dateStartInput) &&
+              exerciseLog.date <= new Date(dateEndInput)
             );
           });
-          // limit output to 10 logs.
+
+          // const input = [...filteredLogs];
+
+          // // input[0]['duration'] = 2;
+
+          // // console.log(input[0]['duration']);
+
+          // delete input['duration'];
+
+          // console.log(input);
+
+          // // format the date in to be presented into dd/mm/yyyy
+          // input.forEach(exerciseLog => {
+          //   // need to remove as it only accepts date objects so can't overwrite with new dd/mm/yyyy format
+          //   delete exerciseLog.date;
+
+          //   // let editDt =
+          //   //   dt.getDate() + '/' + (dt.getMonth() + 1) + '/' + dt.getFullYear();
+
+          //   // // reintroduce the date key
+          //   // exerciseLog.date = editDt;
+          // });
+
+          // // console.log(input);
+
+          // // limit results to first 10 results;
           res.send({
             username: usernameInput,
             logs: filteredLogs.slice(0, 10)
@@ -199,5 +217,46 @@ app.get('/api/exercise/log?', (req, res) => {
 // }).exec((err, data) => {
 //   if (data) {
 //     res.send(data);
+//   }
+// });
+
+// // get user name document
+// Users.findOne({ username: usernameInput }, (err, data) => {
+//   // if existing get data
+//   if (data) {
+//     // extract logs array
+//     const logs = data.logs;
+
+//     // if user did not provide both dates return first 10 logs
+//     if (dateStartInput === '' && dateEndInput === '') {
+//       res.send({ username: usernameInput, logs: logs.slice(0, 10) });
+//     }
+//     // if provided either start date or end date
+//     else {
+//       // if no start date provided then set to earliest date possilbe
+//       if (dateStartInput === '') {
+//         dateStartInput = new Date(1970 - 01 - 01);
+//       }
+
+//       // if no end date provided set to latest date
+//       if (dateEndInput === '') {
+//         dateEndInput = new Date();
+//       }
+
+//       // then go Filter array by date
+//       let filteredLogs = logs.filter(exerciseLog => {
+//         return (
+//           exerciseLog.date >= new Date(dateStartInput) &&
+//           exerciseLog.date <= new Date(dateEndInput)
+//         );
+//       });
+
+//       // limit results to first 10 results;
+//       res.send({ username: usernameInput, logs: filteredLogs.slice(0, 10) });
+//     }
+//   }
+//   // if user is not in database
+//   else {
+//     res.send('Username does not exist');
 //   }
 // });

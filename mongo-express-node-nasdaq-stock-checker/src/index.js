91ff6991ef2dfac0577155de26839898f4cb08dd
niccololampa <niccololampa@gmail.com>
2018-12-27 19:24:23 +0800

    updates-work-on-going

--- mongo-express-node-nasdaq-stock-checker/src/index.js
+++ mongo-express-node-nasdaq-stock-checker/src/index.js
@@ -9,21 +9,23 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const request = require('request');
 // for put and delete form methods
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 
 const app = express();
 
-//  express app to set 'pug' as the 'view-engine'. 
-app.set('view engine', 'pug')
+//  express app to set 'pug' as the 'view-engine'.
+app.set('view engine', 'pug');
+
+app.set('trust proxy', true);
 
 // this will include all helmet middleware
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.noCache());
 
-
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 const { Schema } = mongoose;
 
@@ -40,162 +42,168 @@ app.use(express.static(`${__dirname}/public`));
 // GETTING READY FOR POST REQUEST
 app.use('/', bodyParser.urlencoded({ extended: false }));
 
-
-// // // -------------------------------------------------------------------------
-// // // create a STOCK LIKE SCHEMA 
-
-// const stockLikeSchema = new Schema({
-//   comment_by: { type: String, required: true },
-//   comment_text: { type: String, required: true },
-//   comment_date: { type: Date, required: true }
-// })
-
 // -------------------------------------------------------------------------
-// CREATE  A BOOK SCHEMA
+// CREATE  A STOCK SCHEMA
 const stocksSchema = new Schema({
   stock_symbol: { type: String, required: true },
-  stock_likes: { type: Array, "default": [], required: true }
+  stock_likes: { type: Array, default: [], required: true }
 });
 
 const Stocks = mongoose.model('Stocks', stocksSchema);
 
-
 // ----------------------------------------------------------------------------------------
 // GET HOME PAGE
 app.get('/', (req, res) => {
-  Stocks.find().select('-__v').sort({ created_on: -1 }).exec((err, data) => {
-    if (data) {
-      res.render(`${__dirname}/views/pug/index.pug`, { booksDocs: data });
-    } else {
-      res.render("Error Loading Home Page")
-    }
-  })
+  Stocks.find()
+    .select('-__v')
+    .sort({ created_on: -1 })
+    .exec((err, data) => {
+      if (data) {
+        res.render(`${__dirname}/views/pug/index.pug`, { booksDocs: data });
+      } else {
+        res.render('Error Loading Home Page');
+      }
+    });
 });
 
+// -------------------------------------------------------------------------
+//  GETTING A API REQUEST
+
+const getStockPrice = stock => {
+  const stockURL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${
+    process.env.API_KEY
+  }`;
+
+  return new Promise((resolve, reject) => {
+    request(stockURL, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      // res.send(JSON.parse(body))
+      // res.send(JSON.parse(body)["Global Quote"]["05. price"])
+      resolve(JSON.parse(body));
+    });
+  });
+};
+
 // ----------------------------------------------------------------------------------------
-//  PROCESS GET FOR BOOK, input is ID
+// CHECK IF IP IS INCLUDED IN STOCK'S LIKES ALREADY
+
+const ipExistCheck = (stock, ip) => {
+  Stocks.findOne({ stock_symbol: stock }, (err, data) => {
+    if (data) {
+      if (data.stock_likes.indexOf(ip) > -1) {
+        return true;
+      }
+      return false;
+    }
+
+    return false;
+  });
+};
+
+// ----------------------------------------------------------------------------------------
+//  CHECK IF STOCK IN THE DATABASE IF LIKED. IF YES UPSERT DATABASE
+
+const updateLikesDatabase = (stock, like, ip) => {
+  console.log('updating');
+  // upload if existing and if liked insert IP address to stock_likes list
+
+  const status = ipExistCheck(stock, ip);
+
+  Stocks.updateOne(
+    { stock_symbol: stock },
+    like === 'true' ? { $push: { stock_likes: ip } } : {},
+    { upsert: true },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+// ----------------------------------------------------------------------------------------
+//  PROCESS GET FOR STOCK input is ID
 
 app.get('/api/stock-prices?', (req, res) => {
   // { stock: [ 'dsf', 'aadsf' ], like: 'true' }
-  // console.log(req.query)  
+  let { stock, like } = req.query;
+  const prices = [];
+  const stockLikes = [];
 
-  const { stock, like } = req.query;
+  // IP
 
-  // if user likes stock
-  if (like === "true") {
-    // post Stock in the database
-
-    const stockUploaded = new Stocks({ stock_symbol: stock, stock_likes: ['test'] })
-    stockUploaded.save((err, data) => {
-      if (data) {
-        res.json({ stock_symbol: data.stock_symbol, likes: data.stock_likes.length })
-      }
-      else {
-        res.send("upload error")
-      }
-    });
+  let { ip } = req;
+  if (ip.substr(0, 7) === '::ffff:') {
+    ip = ip.substr(7);
   }
 
-  // Books.findOne({ _id: req.params.id_string }, (err, data) => {
-  //   if (data) {
-  //     // res.send(data)
-  //     res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: data })
-  //   }
-  //   // if id does not exist in database
-  //   else {
-  //     res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: null })
-  //     // res.send("Issue Id does not exist")
-  //   }
-  // })
-})
+  // if single stock request then make it a stock a list so that stock[0] = stock for API request
+  if (typeof stock === 'string') {
+    stock = [stock];
+  }
 
-
-// ---------------------------------------------------------------------
-//  PROCESS POST REQUEST. WHEN NEW BOOK IS SUBMITTED
-app.post('/api/books/', (req, res) => {
-  // get the url provided in form
-  const { bookTitle, bookAuthor, bookDetails } = req.body;
-  const dateCreated = new Date();
-
-  // Upload submitted Issue to Mongo database
-  const booksUploaded = new Books({ book_title: bookTitle, book_author: bookAuthor, book_details: bookDetails, created_on: dateCreated });
-  booksUploaded.save((err, data) => {
-    if (data) {
-      res.redirect(`/api/books/${data._id}`)
-    }
-    else {
-      res.send("upload error")
-    }
-  });
-
-});
-
-
-// ----------------------------------------------------------------------------------------
-// UPLOAD COMMENT TO BOOK USING PUT 
-
-app.put('/api/books/:id_string?', (req, res) => {
-
-  const { bookId, commentBy, commentText } = req.body
-
-  const commentDate = new Date();
-
-  const commentsData = { comment_by: commentBy, comment_text: commentText, comment_date: commentDate };
-
-  Books.findOne({ _id: bookId }, (err, data) => {
-    // if id exist
-    if (data) {
-      Books.update({ _id: bookId }, { $push: { book_comments: commentsData } }, (error, updated) => {
-        if (updated) {
-          res.redirect(`/api/books/${data._id}`)
+  getStockPrice(stock[0]).then(
+    //
+    response => {
+      // extract current price from json(API response)
+      const stockPrice = response['Global Quote']['05. price'];
+      // check if stockPrice is valid(if stock is not existing in API it will be undefined)
+      if (stockPrice !== undefined) {
+        // call the updatelikes database function. Which will update the database if stock is liked
+        updateLikesDatabase(stock[0], like, ip);
+      }
+      // Once database is updated find the stock symbol
+      Stocks.findOne({ stock_symbol: stock[0] }, (err, data) => {
+        if (data) {
+          res.json({
+            stockData: {
+              stock: stock[0],
+              price:
+                stockPrice === undefined
+                  ? 'Stock Symbol Not Valid'
+                  : stockPrice,
+              likes: data.stock_likes.length
+            }
+          });
+        } else {
+          console.log('erorro in stock fin');
         }
-        else {
-          res.render(`${__dirname}/views/pug/comment-result.pug`, { updateData: null })
-        }
-      })
+      });
+    },
+
+    // error in the async
+    error => {
+      console.log(error);
     }
-    // if id does not exist  
-    else {
-      res.render(`${__dirname}/views/pug/comment-result.pug`, { updateData: "no-id" })
-    }
-  })
+  );
 
-})
-
-// ----------------------------------------------------------------------------------------
-// DELETE BOOKS
-
-
-// delete all books
-app.delete('/api/books?', (req, res) => {
-  Books.deleteMany({}, (err, data) => {
-    // if succesful in deleting entire library of books
-    if (data) {
-      res.render(`${__dirname}/views/pug/library-delete.pug`, { libraryDeleted: true });
-      // if id not existing
-    } else {
-      res.render(`${__dirname}/views/pug/library-delete.pug`, { libraryDeleted: false });
-    }
-  })
-
-})
-
-
-// delete single book
-app.delete('/api/books/:id_string?', (req, res) => {
-  Books.deleteOne({ _id: req.body.bookId }, (err, data) => {
-    // if id is existing delete book
-    if (data) {
-      res.render(`${__dirname}/views/pug/book-delete.pug`, { bookDeleted: true });
-      // if id not existing
-    } else {
-      res.render(`${__dirname}/views/pug/book-delete.pug`, { bookDeleted: false });
-    }
-  });
+  // send JSON
 });
-
-
-
 
 /* Coded by Niccolo Lampa. Email: niccololampa@gmail.com */
 
+// Books.findOne({ _id: req.params.id_string }, (err, data) => {
+//   if (data) {
+//     // res.send(data)
+//     res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: data })
+//   }
+//   // if id does not exist in database
+//   else {
+//     res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: null })
+//     // res.send("Issue Id does not exist")
+//   }
+// })
+
+// // post Stock in the database
+// const stockUploaded = new Stocks({ stock_symbol: stock, stock_likes: [] })
+// stockUploaded.save((err, data) => {
+//   if (err) {
+//     res.send("upload error")
+//   }
+//   else {
+//     // res.json({ stock_symbol: data.stock_symbol, likes: data.stock_likes.length })
+//     stockLikes.push(data.stock_likes.length)
+//   }
+// });

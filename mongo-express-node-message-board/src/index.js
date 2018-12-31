62f3bac8acd9224039b9f6da2888dcf81c8d9fdf
niccololampa <niccololampa@gmail.com>
2018-12-31 23:56:32 +0800

    updates-to-project

--- mongo-express-node-message-board/src/index.js
+++ mongo-express-node-message-board/src/index.js
@@ -10,20 +10,22 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // for put and delete form methods
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+
+// to hash password for deleting
+// const bcrypt = require('bcrypt');
 
 const app = express();
 
-//  express app to set 'pug' as the 'view-engine'. 
-app.set('view engine', 'pug')
+//  express app to set 'pug' as the 'view-engine'.
+app.set('view engine', 'pug');
 
 // this will include all helmet middleware
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.noCache());
 
-
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 const { Schema } = mongoose;
 
@@ -40,147 +42,219 @@ app.use(express.static(`${__dirname}/public`));
 // GETTING READY FOR POST REQUEST
 app.use('/', bodyParser.urlencoded({ extended: false }));
 
-
 // -------------------------------------------------------------------------
-// create a COMMENT SCHEMA 
+// CREATE A REPLIES SCHEMA
 
-const commentsSchema = new Schema({
-  comment_by: { type: String, required: true },
-  comment_text: { type: String, required: true },
-  comment_date: { type: Date, required: true }
-})
-
-// -------------------------------------------------------------------------
-// CREATE  A BOOK SCHEMA
-const booksSchema = new Schema({
-  book_title: { type: String, required: true },
-  book_author: { type: String, required: true },
-  book_details: { type: String, required: true },
-  book_comments: { type: [commentsSchema] },
-  created_on: { type: Date }
-
+const replySchema = new Schema({
+  reply_by: { type: String, required: true },
+  reply_text: { type: String, required: true },
+  reply_date: { type: Date, required: true },
+  reply_reports: { type: Number, default: 0 },
+  reply_password: { type: String, required: true }
 });
 
-const Books = mongoose.model('Books', booksSchema);
+const Replies = mongoose.model('replies', replySchema);
 
+// -------------------------------------------------------------------------
+// CREATE A THREAD SCHEMA
+const threadSchema = new Schema({
+  thread_title: { type: String, required: true },
+  thread_author: { type: String, required: true },
+  thread_details: { type: String, required: true },
+  thread_replies: { type: [replySchema] },
+  thread_reports: { type: Number, default: 0 },
+  thread_date: { type: Date },
+  thread_password: { type: String, required: true }
+});
+
+const Threads = mongoose.model('threads', threadSchema);
 
 // ----------------------------------------------------------------------------------------
 // GET HOME PAGE
 app.get('/', (req, res) => {
-  Books.find().select('-__v').sort({ created_on: -1 }).exec((err, data) => {
-    if (data) {
-      res.render(`${__dirname}/views/pug/index.pug`, { booksDocs: data });
-    } else {
-      res.render("Error Loading Home Page")
-    }
-  })
+  Threads.find()
+    .select('-__v')
+    .sort({ created_on: -1 })
+    .exec((err, data) => {
+      if (data) {
+        res.render(`${__dirname}/views/pug/index.pug`, { threadDocs: data });
+      } else {
+        res.render('Error Loading Home Page');
+      }
+    });
 });
 
 // ----------------------------------------------------------------------------------------
 //  PROCESS GET FOR BOOK, input is ID
 
-app.get('/api/books/:id_string', (req, res) => {
-
-  Books.findOne({ _id: req.params.id_string }, (err, data) => {
+app.get('/api/threads/:board', (req, res) => {
+  Threads.findOne({ _id: req.params.board }, (err, data) => {
     if (data) {
       // res.send(data)
-      res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: data })
+      res.render(`${__dirname}/views/pug/thread-info.pug`, {
+        threadData: data
+      });
     }
     // if id does not exist in database
     else {
-      res.render(`${__dirname}/views/pug/book-info.pug`, { bookData: null })
+      res.render(`${__dirname}/views/pug/thread-info.pug`, {
+        threadData: null
+      });
       // res.send("Issue Id does not exist")
     }
-  })
-})
-
+  });
+});
 
 // ---------------------------------------------------------------------
-//  PROCESS POST REQUEST. WHEN NEW BOOK IS SUBMITTED
-app.post('/api/books/', (req, res) => {
+//  PROCESS POST REQUEST. WHEN NEW THREAD IS SUBMITTED
+app.post('/api/threads/', (req, res) => {
   // get the url provided in form
-  const { bookTitle, bookAuthor, bookDetails } = req.body;
+  const { threadTitle, threadAuthor, threadDetails, threadPassword } = req.body;
   const dateCreated = new Date();
 
   // Upload submitted Issue to Mongo database
-  const booksUploaded = new Books({ book_title: bookTitle, book_author: bookAuthor, book_details: bookDetails, created_on: dateCreated });
-  booksUploaded.save((err, data) => {
+  const threadUploaded = new Threads({
+    thread_title: threadTitle,
+    thread_author: threadAuthor,
+    thread_details: threadDetails,
+    thread_date: dateCreated,
+    thread_password: threadPassword
+  });
+  threadUploaded.save((err, data) => {
     if (data) {
-      res.redirect(`/api/books/${data._id}`)
-    }
-    else {
-      res.send("upload error")
+      res.redirect(`/api/threads/${data._id}`);
+    } else {
+      res.send('upload error');
     }
   });
-
 });
 
-
 // ----------------------------------------------------------------------------------------
-// UPLOAD COMMENT TO BOOK USING PUT 
+// UPLOAD REPORT TO THREAD USING PUT
 
-app.put('/api/books/:id_string?', (req, res) => {
+app.put('/api/threads/:board?', (req, res) => {
+  const { threadId } = req.body;
 
-  const { bookId, commentBy, commentText } = req.body
-
-  const commentDate = new Date();
-
-  const commentsData = { comment_by: commentBy, comment_text: commentText, comment_date: commentDate };
-
-  Books.findOne({ _id: bookId }, (err, data) => {
+  Threads.findOne({ _id: threadId }, (err, data) => {
     // if id exist
     if (data) {
-      Books.update({ _id: bookId }, { $push: { book_comments: commentsData } }, (error, updated) => {
-        if (updated) {
-          res.redirect(`/api/books/${data._id}`)
+      Threads.updateOne(
+        { _id: threadId },
+        { $inc: { thread_reports: 1 } },
+        (error, updated) => {
+          if (updated) {
+            res.redirect(`/api/threads/${data._id}`);
+          } else {
+            res.render(`${__dirname}/views/pug/report-result.pug`, {
+              updateData: null
+            });
+          }
         }
-        else {
-          res.render(`${__dirname}/views/pug/comment-result.pug`, { updateData: null })
-        }
-      })
+      );
     }
-    // if id does not exist  
+    // if id does not exist
     else {
-      res.render(`${__dirname}/views/pug/comment-result.pug`, { updateData: "no-id" })
-    }
-  })
-
-})
-
-// ----------------------------------------------------------------------------------------
-// DELETE BOOKS
-
-
-// delete all books
-app.delete('/api/books?', (req, res) => {
-  Books.deleteMany({}, (err, data) => {
-    // if succesful in deleting entire library of books
-    if (data) {
-      res.render(`${__dirname}/views/pug/library-delete.pug`, { libraryDeleted: true });
-      // if id not existing
-    } else {
-      res.render(`${__dirname}/views/pug/library-delete.pug`, { libraryDeleted: false });
-    }
-  })
-
-})
-
-
-// delete single book
-app.delete('/api/books/:id_string?', (req, res) => {
-  Books.deleteOne({ _id: req.body.bookId }, (err, data) => {
-    // if id is existing delete book
-    if (data) {
-      res.render(`${__dirname}/views/pug/book-delete.pug`, { bookDeleted: true });
-      // if id not existing
-    } else {
-      res.render(`${__dirname}/views/pug/book-delete.pug`, { bookDeleted: false });
+      res.render(`${__dirname}/views/pug/report-result.pug`, {
+        updateData: 'no-id'
+      });
     }
   });
 });
 
+// ----------------------------------------------------------------------------------------
+// DELETE THREAD
 
+app.delete('/api/threads/:board?', (req, res) => {
+  const { threadId, threadPassword } = req.body;
 
+  Threads.findOne({ _id: threadId }, (err, data) => {
+    if (data) {
+      if (threadPassword !== data.thread_password) {
+        res.send('Password to delete thread invalid');
+        return;
+      }
+
+      Threads.deleteOne({ _id: threadId }, (error, deldata) => {
+        // if id is existing delete thread
+        if (deldata) {
+          res.render(`${__dirname}/views/pug/thread-delete.pug`, {
+            threadDeleted: true
+          });
+          // if id not existing
+        } else {
+          res.render(`${__dirname}/views/pug/thread-delete.pug`, {
+            threadDeleted: false
+          });
+        }
+      });
+    } else {
+      res.render(`${__dirname}/views/pug/thread-delete.pug`, {
+        threadDeleted: false
+      });
+    }
+  });
+});
+
+// ---------------------------------------------------------------------
+//  PROCESS POST REQUEST. WHEN NEW REPLY IS SUBMITTED
+app.post('/api/replies/:board?', (req, res) => {
+  // get the url provided in form
+  const { replyAuthor, replyDetails, replyPassword, threadId } = req.body;
+  const dateCreated = new Date();
+
+  console.log(threadId);
+
+  // Upload submitted Issue to Mongo database
+  const replyUploaded = new Replies({
+    reply_by: replyAuthor,
+    reply_text: replyDetails,
+    reply_date: dateCreated,
+    reply_password: replyPassword
+  });
+
+  Threads.updateOne(
+    { _id: threadId },
+    { $push: { thread_replies: replyUploaded } },
+    (error, data) => {
+      if (data) {
+        res.redirect(`/api/threads/${data._id}`);
+      } else {
+        res.send('Posting Reply Failed');
+      }
+    }
+  );
+});
+
+// ---------------------------------------------------------------------
+// UPLOAD REPORT REPLY USING PUT
+
+app.put('/api/replies/:board?', (req, res) => {
+  const { threadId, replyId } = req.body;
+
+  Threads.findOne({ _id: threadId }, (err, data) => {
+    // if id exist
+    if (data) {
+      Threads.updateOne(
+        { _id: threadId, "thread_replies._id": },
+        { $inc: { thread_replies: { _id: replyId, reply_reports: 1 } } },
+        (error, updated) => {
+          if (updated) {
+            res.redirect(`/api/threads/${data._id}`);
+          } else {
+            res.render(`${__dirname}/views/pug/report-result.pug`, {
+              updateData: null
+            });
+          }
+        }
+      );
+    }
+    // if id does not exist
+    else {
+      res.render(`${__dirname}/views/pug/report-result.pug`, {
+        updateData: 'no-id'
+      });
+    }
+  });
+});
 
 /* Coded by Niccolo Lampa. Email: niccololampa@gmail.com */
-
--- mongo-express-node-message-board/src/public/js/form.js
+++ mongo-express-node-message-board/src/public/js/form.js
@@ -1,26 +1,21 @@
 function urlCreatorGet() {
-    const updateId = document.getElementById('getId').value;
-    const action_src =
-        `/api/books/${updateId}`;
-    const url_form = document.getElementById('getBookForm');
-    url_form.action = action_src;
-
+  const updateId = document.getElementById('getId').value;
+  const action_src = `/api/threads/${updateId}`;
+  const url_form = document.getElementById('getThreadForm');
+  url_form.action = action_src;
 }
 
 function urlCreatorPut() {
-    const updateId = document.getElementById('updateId').value;
-    const action_src =
-        `/api/books/${updateId}?_method=PUT`;
-    const url_form = document.getElementById('commentBookForm');
-    url_form.action = action_src;
+  const updateId = document.getElementById('updateId').value;
+  const action_src = `/api/threads/${updateId}?_method=PUT`;
+  const url_form = document.getElementById('reportThreadForm');
+  url_form.action = action_src;
 }
 
 function urlCreatorDel() {
-    const updateId = document.getElementById('deleteId').value;
+  const updateId = document.getElementById('deleteId').value;
 
-    const action_src =
-        `/api/books/${updateId}?_method=DELETE`;
-    const url_form = document.getElementById('deleteBookForm');
-    url_form.action = action_src;
-
+  const action_src = `/api/threads/${updateId}?_method=DELETE`;
+  const url_form = document.getElementById('deleteThreadForm');
+  url_form.action = action_src;
 }
